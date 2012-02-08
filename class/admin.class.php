7de0ab1f3998f42eee25367b8f07b94264cfd04f
Hauke Schade <cnlpete@cnlpete.de>
2012-02-08 18:25:11 +0100

    fixing bug when isnerting testdata

--- class/admin.class.php
+++ class/admin.class.php
@@ -159,7 +159,7 @@ EOD;
 			$result = DB::execute($sql);
 
 			if ($result && $insertData) {
-				Admin::insertTestData('colorclass');
+				Admin::insertTestData('colorclasses');
 			}
 			return $result;
 		}
@@ -312,7 +312,7 @@ EOD;
 			case 'events':
 			$sql = <<<EOD
 INSERT INTO `events` (`title`, `startdate`, `enddate`, `details`, `colorclass`) VALUES
-('noch ein langes Ereignis &uuml;ber mehrere Jahre', '1940-03-01', 1944-03-01', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Loremclita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 'Religion'),
+('noch ein langes Ereignis &uuml;ber mehrere Jahre', '1940-03-01', '1944-03-01', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Loremclita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 'Religion'),
 ('Test-Event', '1952-03-01', '0000-00-00', 'dieses event hat kein enddatum', 'Politik'),
 ('aufregend', '1942-00-00', '1942-00-00', 'Dieses Event hört im gleichen jahr auf, wie es anfängt. es hat zudem keine moats angabe, bzw. tages angabe', 'Wissenschaft'),
 ('ein event und so', '1938-03-01', '1943-03-01', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 'Religion'),
